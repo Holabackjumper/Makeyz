ae003df174b908527a376f976fd99babc7838900
Jonatan Lindahl <JonatanLindahl@Jonatans-MacBook-Pro.local>
2014-04-11 14:42:00 +0200

    Initial Commit

---
+++ Makemoves/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Makemoves
+//
+//  Created by Jonatan Lindahl on 2014-04-11.
+//  Copyright (c) 2014 Uzibazooka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
